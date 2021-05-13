e5a40287ff988a76ca1f13a1b2e75dd6af22c0f6
suwon <jeroschoi@naver.com>
2021-05-13 13:27:11 +0900

    1. Rabbit MQ ErrorHandler 및 Thread 처리 추가 2. DependsOn Annotation 추가

--- src/main/java/com/msg/gw/config/RabbitMqConfig.java
+++ src/main/java/com/msg/gw/config/RabbitMqConfig.java
@@ -9,39 +9,50 @@ import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.rabbit.listener.FatalExceptionStrategy;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.util.ErrorHandler;
 
-@Configurable
+
+@Configuration
+@DependsOn(value = {"appConfig"})
 public class RabbitMqConfig {
 
     private Logger log = (Logger) LoggerFactory.getLogger(this.getClass());
-    private IConsumer consumer;
 
     public void sendData(String ConsumerType , Message sendMsg) throws Exception{
-        this.consumer = new ConsumerFactory().getInstance(ConsumerType);
-        if(this.consumer != null)
-            this.consumer.sendData(sendMsg);
+        IConsumer consumer = new ConsumerFactory().getInstance(ConsumerType);
+        if(consumer != null)
+            consumer.sendData(sendMsg);
     }
 
     public ConnectionFactory rqConnectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("127.0.0.1");
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        connectionFactory.setVirtualHost("/");
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory((String)AppConfig.getConfig().get("MQ.URL"));
+        connectionFactory.setUsername((String)AppConfig.getConfig().get("MQ.USER"));
+        connectionFactory.setPassword((String)AppConfig.getConfig().get("MQ.PASS"));
+        connectionFactory.setVirtualHost((String)AppConfig.getConfig().get("MQ.VIR.HOST"));
         return connectionFactory;
     }
 
     @Bean
     public SimpleMessageListenerContainer msgListenerContainer(){
+        log.debug(" CHECK THE Spring!!! ");
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(rqConnectionFactory());
-        container.setQueueNames("TEST");
+        container.setQueueNames((String)AppConfig.getConfig().get("MQ.QUEUE"));
         container.setMessageListener(executeMessage());
+        // Listener Thread 처리
+        //container.setConcurrentConsumers(10);
+        container.setErrorHandler(errorProcess());
         return container;
     }
 
+    @Bean
     public MessageListener executeMessage(){
         return new MessageListener() {
             @Override
@@ -58,6 +69,29 @@ public class RabbitMqConfig {
                 }
             }
         };
+    }
+
+    // Error Handler 추가
+    public ErrorHandler errorProcess(){
+        return new ConditionalRejectingErrorHandler(new FatalExceptionStrategy() {
+            @Override
+            public boolean isFatal(Throwable throwE) {
+                // error 처리 추가
+                if(throwE instanceof ListenerExecutionFailedException){
+                    ListenerExecutionFailedException lefE = (ListenerExecutionFailedException) throwE;
+                    Message msg = lefE.getFailedMessage();
+                    // msg 처리로직
+                    try {
+                        log.debug("Filed msg : - " + new String(msg.getBody(), "UTF-8"));
+                        // 로그 DB 저장 시 DB Consumer 사용 or Slack 전송
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                // false 일 경우 모든 Thread에 전달
+                return true;
+            }
+        });
     }
 
 }
