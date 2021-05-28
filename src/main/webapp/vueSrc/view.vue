cd4f9737b81c869d410f59a632ec8f3ca2e4b1bb
suwon <jeroschoi@naver.com>
2021-05-28 13:22:41 +0900

    view 관련 화면 추가

---
+++ src/main/webapp/vueSrc/view.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="view">
+    <AUIGridSample/>
+  </div>
+</template>
+
+<script>
+// AUIGridSample 등록
+import GridExample from './VeuGridExample.vue'
+export default {
+  name: 'view',
+  components: {
+    GridExample
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
