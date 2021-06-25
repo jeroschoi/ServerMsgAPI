2a53c9944083c01f74156f760d300a13808f8975
suwon <jeroschoi@naver.com>
2021-06-25 09:44:20 +0900

    Vue Js Axios이용 Put Get 처리 추가

--- src/main/java/com/msg/receiver/controller/sampleController.java
+++ src/main/java/com/msg/receiver/controller/sampleController.java
@@ -5,10 +5,7 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import com.msg.gw.config.AppConfig;
 import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class sampleController {
@@ -16,8 +13,8 @@ public class sampleController {
 	private static final String logLevel = "DEBUG";
 
 	@ResponseBody
-	@RequestMapping(value="/putHello.do" , method={RequestMethod.GET ,RequestMethod.POST} , produces= {"application/json"})
-	public String HelloWolrd(HttpServletRequest req) {
+	@RequestMapping(value="/get.do" , method={RequestMethod.GET ,RequestMethod.POST} , produces="application/json;charset=UTF-8")
+	public String getProp(HttpServletRequest req) {
         Map prop = AppConfig.getConfig();
 	    try {
             System.out.println(prop.toString());
@@ -30,5 +27,19 @@ public class sampleController {
 	    System.out.println("CHECK : " + json.toJSONString());
 
 	    return json.toJSONString();
+    }
+
+
+    @PostMapping(value="/put.do", produces="application/json;charset=UTF-8")
+    public String putProp(@RequestBody Map<String, Object> payload) {
+        Map prop = AppConfig.getConfig();
+        String name = (String)payload.get("name");
+        String value = (String)payload.get("value");
+        try {
+            prop.put(name , value );
+            System.out.println(prop.toString());
+        }catch (Exception e){
+        }
+        return payload.toString();
     }
 }
